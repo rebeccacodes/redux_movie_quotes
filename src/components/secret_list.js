12679dd7e3ed825bf0c81b7abe5a04ccc22301d0
Rebecca Martin <rebecca-martin@live.com>
2018-08-29 17:59:19 -0700

    Routing setup complete

---
+++ src/components/secret_list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">Secret List</h1>
+        <ol>
+            <li>Tom</li>
+            <li>Jack</li>
+            <li>Harry</li>
+            <li>Sally</li>
+            <li>Bob</li>
+        </ol>
+
+    </div>
+);
+
